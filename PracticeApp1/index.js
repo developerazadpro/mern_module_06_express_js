343303498818b1dfca07f92f5f696c1dfdb35521
developerazadpro <azharul.ece13.hstu@gmail.com>
2023-05-06 01:13:07 +0600

    index file edit

--- PracticeApp1/index.js
+++ PracticeApp1/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const port = 8000;
+const app = express();
 
-app = express();
-app.get('/', function (req, res){
+app.get('/',  (req, res) => {
     res.send("Hello Express JS");
 });
 
-app.listen(8000, function (){
+app.listen(port,  () =>{
     console.log("Server Run Success.")
 })

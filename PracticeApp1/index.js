b644c5bfb48e7204d93dc5190dd2b6643b4d7c1a
developerazadpro <azharul.ece13.hstu@gmail.com>
2023-05-06 01:33:40 +0600

    basic routing

--- PracticeApp1/index.js
+++ PracticeApp1/index.js
@@ -6,6 +6,14 @@ app.get('/',  (req, res) => {
     res.send("Hello Express JS");
 });
 
+app.post('/about',  (req, res) => {
+    res.send("About Page");
+});
+
+app.put('/contact',  (req, res) => {
+    res.send("Contact Page");
+});
+
 app.listen(port,  () =>{
     console.log("Server Run Success.")
 })
